c4296046b97747e3d020cb7bb14cfe39b2e911f5
Hyeongjin Kim <hyeongjin184@gmail.com>
2017-06-13 11:43:34 +0900

    오전

--- django_app/post/urls.py
+++ django_app/post/urls.py
@@ -1,6 +1,16 @@
 from django.conf.urls import url
 from . import views
 
+# url namespace
+app_name = 'post'
 urlpatterns = [
-    url(r'^$', views.post_list),
+    # /post/$
+    url(r'^$', views.post_list, name='post_list'),
+    # /post/3/$, /post/35/$
+    # 정규표현식에서 매칭된 그룹을 위치인수로 반환하는 방법
+    # url(r'^(\d+)/$', views.post_detail),
+
+    # 정규표현식에서 매칭된 그룹을 키워드인수로 반환하는 방법
+    # 그룹의 가장 앞 부분에 ?P<패턴이름>을 지정
+    url(r'^(?P<post_pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
--- django_app/post/views.py
+++ django_app/post/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseNotFound
+from django.shortcuts import render, redirect
+from django.template import loader
+
 from .models import Post
 
 
@@ -8,3 +12,50 @@ def post_list(request):
         'posts': posts,
     }
     return render(request, 'post/post_list.html', context)
+
+
+def post_detail(request, post_pk):
+    # Model(DB)에서 post_pk에 해당하는 Post객체를 가져와 변수에 할당
+    # ModelManager의 get메서드를 사용해서 단 한개의 객체만 가져온다
+
+    # 가져오는 과정에서 예외처리를 한다 (Model.DoesNotExist)
+    try:
+        post = Post.objects.get(pk=post_pk)
+    except Post.DoesNotExist as e:
+        # 1. 404 NotFound를 띄워준다
+        # return HttpResponseNotFound('Post not found, detail: {}'.format(e))
+
+        # 2. post_list view로 돌아간다
+        # redirect
+        return redirect('post:post_list')
+
+    # request에 대해 response를 돌려줄때는 HttpResponse나 render를 사용가능
+    # template을 사용하려면 render함수를 사용한다
+    # render함수는
+    #   django.template.loader.get_template함수와
+    #   django.http.HttpResponse함수를 축약해 놓은 shortcut이다
+
+    # Django가 템플릿을 검색할 수 있는 모든 디렉토리를 순회하며
+    # 인자로 주어진 문자열값과 일치하는 템플릿이 있는지 확인 후
+    # 결과를 리턴 (django.templates.backends.django.Template클래스형 객체)
+    template = loader.get_template('post/post_detail.html')
+    # dict형 변수 context의 'post'키에 post(Post객체)를 할당
+    context = {
+        'post': post,
+    }
+    # template에 인자로 주어진 context, request를 render함수를 사용해서 해당 template을 string으로 변환
+    rendered_string = template.render(context=context, request=request)
+    # 변환된 string을 HttpResponse형태로 돌려준다
+    return HttpResponse(rendered_string)
+
+
+def post_create(request):
+    pass
+
+
+def post_modify(request, post_pk):
+    pass
+
+
+def post_delete(request, post_pk):
+    pass
