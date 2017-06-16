1ed7dbd3ade2cf625230cee1ff14ff9b141c7eaf
Hyeongjin Kim <hyeongjin184@gmail.com>
2017-06-16 15:43:08 +0900

    이미 다 섞여있다.

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -91,6 +91,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                'member.context_proccessors.forms',
             ],
         },
     },
--- django_app/member/views.py
+++ django_app/member/views.py
@@ -1,6 +1,10 @@
-from django.contrib.auth import authenticate, login as django_login, logout as django_logout
+from django.contrib.auth import authenticate, login as django_login, logout as django_logout, get_user_model
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+
+from .forms import LoginForm, SignupForm
+
+User = get_user_model()
 
 
 def login(request):
@@ -16,20 +20,34 @@ def login(request):
 
     # POST요청이 왔을 경우
     if request.method == 'POST':
+
+        # Form클래스 미사용시
         # 요청받은 POST데이터에서 username, password키가 가진 값들을
         # username, password변수에 할당 (문자열)
-        username = request.POST['username']
-        password = request.POST['password']
+        # username = request.POST['username']
+        # password = request.POST['password']
         # authenticate함수를 사용해서 User객체를 얻어 user에 할당
         # 인증에 실패할 경우 user변수에는 None이 할당됨
-        user = authenticate(
-            request,
-            username=username,
-            password=password
-        )
-        if user is not None:
+        # user = authenticate(
+        #     request,
+        #     username=username,
+        #     password=password
+        # )
+        #
+        # if user is not None:
+        #     django_login(request, user)
+        #     return redirect('post:post_list')
+
+
+        # Form클래스 사용시
+        #   Bound form생성
+        form = LoginForm(data=request.POST)
+        # Bound form의 유효성을 검증
+        if form.is_valid():
+            user = form.cleaned_data['user']
             django_login(request, user)
             return redirect('post:post_list')
+
         else:
             return HttpResponse('Login credentials invaild')
     else:
@@ -38,7 +56,13 @@ def login(request):
         # 아닐경우 login.html을 render해서 리턴
         if request.user.is_authenticated:
             return redirect('post:post_list')
-        return render(request, 'member/login.html')
+        # LoginForm인스턴스를 생성해서 context에 넘김
+        form = LoginForm()
+        context = {
+            'form': form,
+        }
+        # render시 context에는 LoginForm클래스형 form객체가 포함됨
+        return render(request, 'member/login.html', context)
 
 
 def logout(request):
@@ -56,6 +80,39 @@ def signup(request):
     #   각각의 경우를 검사해서 틀릴경우 오류메세지 리턴
     #   가입에 성공시 로그인시키고 post_list로 리다이렉트
     if request.method == 'POST':
-        pass
+
+        ### Form을 사용하지 않는 경우
+
+        # # username, password1, password2에 POST로 전달받은 데이터를 할당
+        # username = request.POST['username']
+        # password1 = request.POST['password1']
+        # password2 = request.POST['password2']
+        # # username에 해당하는 User가 있는지 검사
+        # if User.objects.filter(username=username).exists():
+        #     # 이미 존재하는 username일경우
+        #     return HttpResponse('중복된 Username이 있따.')
+        # # password1과 password2가 같은지 검사
+        # elif password1 != password2:
+        #     # 다를경우
+        #     return HttpResponse('패스워드가 틀렸따.')
+        # # 위의 두 경우가 아닌 경우 유저를 생성
+        # user = User.objects.create_user(
+        #     username=username,
+        #     password=password1
+        # )
+
+
+        ### Form을 사용한 경우
+
+        form = SignupForm(data=request.POST)
+        if form.is_valid():
+            user = form.create_user()
+            django_login(request, user)
+            return redirect('post:post_list')
+
     else:
-        return render(request, 'member/signup.html')
+        form = SignupForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'member/signup.html', context)
